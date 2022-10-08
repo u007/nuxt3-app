b920e493261c6f210bba4bcc0b7914496fbefee1
james <james@mercstudio.com>
2022-10-08 22:01:03 +0800

    test

---
+++ components/kanban/kanban-list.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="grid grid-cols-1 bg-gray-100 flex-shrink-0">
+    <h2 class="text-center font-bold my-1">{{title}}</h2>
+    <div class="p-2 ">
+      <kanban-task v-for="task in tasks" :key="task.id" v-bind="task" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'TODO',
+  },
+  tasks: {
+    type: Array as PropType<Task[]>,
+    required: true,
+  },
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,60 @@
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import AutoImport from 'unplugin-auto-import/vite'
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  // server: {
+  //   host: '0.0.0.0',
+  //   port: 3800,
+  // },
+  build: {
+    transpile: [
+      // 'lodash-es'
+    ],
+  },
+  modules: [
+    'nuxt-windicss',
+  ],
+  css: [
+    // windi preflight
+    'virtual:windi-base.css',
+    // your stylesheets which overrides the preflight
+    '@/assets/main.css',
+    // windi extras
+    'virtual:windi-components.css',
+    'virtual:windi-utilities.css',
+  ],
+  vite: {
+    ssr: {
+      noExternal: [
+        'ant-design-vue', /vue-i18n/],
+    },
+    plugins: [
+      Components({
+        resolvers: [AntDesignVueResolver()],
+      }),
 
-})
+      AutoImport({
+        eslintrc: {
+          enabled: true,
+        },
+        imports: [
+            'vue',
+            'vue-router',
+            'vue-i18n',
+            'vue/macros',
+            // {
+            //   '~/store/acl': ['useAclStore', 'UserSession'],
+            //   '~/store/alert': ['useAlertStore'],
+            //   '~/store/nav': ['useNavStore'],
+            // },
+          ],
+          dirs: [
+          ],
+          dts: 'auto-imports.d.ts',
+          vueTemplate: true,
+      }),
+    ],
+  },
+});
